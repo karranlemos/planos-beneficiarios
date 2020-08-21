5ee07dc80310aebc689e7a40298b6c375def708f
Karran Lemos <karranlemos@gmail.com>
2020-08-20 21:24:48 -0300

    Crate basic project structure

---
+++ config/init.php
@@ -0,0 +1,5 @@
+<?php
+
+define('__ROOT__', dirname(__DIR__));
+
+require_once __ROOT__.'/pages/form.php';
---
+++ pages/form.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bitix Test - crie seu plano!</title>
+</head>
+<body>
+  
+  <div class="content">
+
+    <div class="floating-container">
+      <div class="floating-content">
+        
+        <form action="" method="post" class="js-gera-plano full-length">
+          
+          <select>
+            <!-- <option value="opcao">Opção</option> -->
+          </select>
+
+          <div class="beneficiarios js-beneficiarios">
+            <!-- <div class="beneficiario">
+            <input type="text" name="nome-beneficiario-<number>">
+            <input type="number" name="idade-beneficiario-<number>">
+            </div> -->
+          </div>
+
+          <button type="button" class="primary js-add-beneficiario">+</button>
+
+          <button type="button" class="js-submit">Enviar</button>
+
+        </form>
+
+      </div>
+    </div>
+
+  </div>
+
+</body>
+</html>
